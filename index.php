dc34a84ff15503f86d9c28f5ceb376fe1b5457e5
Dmytro Trofimenko <trofuk@gmail.com>
2016-04-20 21:02:52 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,311 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="icon" href="/images/favicon.ico">
+		<title>Menu</title>
+		<link href="css/normalize.css" rel="stylesheet">
+		<link href="css/style.css" rel="stylesheet">
+		<link href='https://fonts.googleapis.com/css?family=Open+Sans:600,400,700' rel='stylesheet' type='text/css'>
+	</head>
+	
+	<body>
+		
+		<header>
+			
+		</header>
+		
+		<main class="menu">
+			
+			<div class="nav">
+				<ul class="dropdown">
+					<li class="dropdown-top">
+						<a class="dropdown-top" href="/">Celeron</a>
+						<ul class="dropdown-inside">
+							
+							<ul class="column">
+								<h4 class="title">Sandy Bridge</h4>
+								<li><a href="/">G440</a></li>
+								<li><a href="/">G460</a></li>
+								<li><a href="/">G530T</a></li>
+								<li><a href="/">G530</a></li>
+								<li><a href="/">G540</a></li>
+							</ul>
+							
+							<ul class="column">
+								<h4 class="title">Ivy Bridge</h4>
+								
+							</ul>
+							
+							<ul class="column">	
+								<h4 class="title">Haswell</h4>
+								<li><a href="/">G1820TE</a></li>
+								<li><a href="/">G1820T</a></li>
+								<li><a href="/">G1840T</a></li>
+								<li><a href="/">G1820</a></li>
+								<li><a href="/">G1830</a></li>
+								<li><a href="/">G1840</a></li>
+								<li><a href="/">G1850</a></li>
+							</ul>
+							
+							<ul class="column">	
+								<h4 class="title">Skylake</h4>
+								<li><a href="/">G3900T</a></li>
+								<li><a href="/">G3900</a></li>
+								<li><a href="/">G3920</a></li>
+								
+							</ul>	
+							
+						</ul>
+						
+						
+					</li>
+					
+					<li class="dropdown-top">
+						<a class="dropdown-top" href="/">Pentium</a>
+						<ul class="dropdown-inside">
+							<ul class="column">	
+								<h4 class="title">Sandy Bridge</h4>
+								<li><a href="/">G620T</a></li>
+								<li><a href="/">G630T</a></li>
+								<li><a href="/">G620</a></li>
+								<li><a href="/">G622</a></li>
+								<li><a href="/">G630</a></li>
+								<li><a href="/">G632</a></li>
+								<li><a href="/">G840</a></li>
+								<li><a href="/">G850</a></li>
+								<li><a href="/">G860</a></li>
+							</ul>
+							
+							<ul class="column">	
+								<h4 class="title">Ivy Bridge</h4>
+								
+							</ul>
+							
+							<ul class="column">	
+								<h4 class="title">Haswell</h4>
+								<li><a href="/">G3220T</a></li>
+								<li><a href="/">G3240T</a></li>
+								<li><a href="/">G3250T</a></li>
+								<li><a href="/">G3220</a></li>
+								<li><a href="/">G3240</a></li>
+								<li><a href="/">G3250</a></li>
+								<li><a href="/">G3258</a></li>
+								<li><a href="/">G3320TE</a></li>
+								<li><a href="/">G3420T</a></li>
+								<li><a href="/">G3440T</a></li>
+								<li><a href="/">G3450T</a></li>
+								<li><a href="/">G3420</a></li>
+								<li><a href="/">G3430</a></li>
+								<li><a href="/">G3440</a></li>
+								<li><a href="/">G3450</a></li>
+								<li><a href="/">G3460</a></li>
+								
+							</ul>
+							
+							<ul class="column">	
+								<h4 class="title">Skylake</h4>
+								<li><a href="/">G4400</a></li>
+								<li><a href="/">G4500</a></li>
+								<li><a href="/">G4520</a></li>
+								
+							</ul>
+						</ul>
+					</li>
+					
+					<li class="dropdown-top">
+						<a class="dropdown-top" href="/">Core i3</a>
+						<ul class="dropdown-inside">
+							
+							<ul class="column">
+								<h4 class="title">Sandy Bridge</h4>
+								<li><a href="/">2100</a></li>
+								<li><a href="/">2102</a></li>
+								<li><a href="/">2105</a></li>
+								<li><a href="/">2120</a></li>
+								<li><a href="/">2125</a></li>
+								<li><a href="/">2130</a></li>
+								<li><a href="/">2100T</a></li>
+								<li><a href="/">2120T</a></li>
+							</ul>
+							
+							<ul class="column">
+								<h4 class="title">Ivy Bridge</h4>
+								<li><a href="/">3220</a></li>
+								<li><a href="/">3225</a></li>
+								<li><a href="/">3240</a></li>
+								
+							</ul>
+							
+							<ul class="column">
+								<h4 class="title">Haswell</h4>
+								<li><a href="/">4130T</a></li>
+								<li><a href="/">4150T</a></li>
+								<li><a href="/">4160T</a></li>
+								<li><a href="/">4130</a></li>
+								<li><a href="/">4150</a></li>
+								<li><a href="/">4160</a></li>
+								<li><a href="/">4330TE</a></li>
+								<li><a href="/">4340TE</a></li>
+								<li><a href="/">4330T</a></li>
+								<li><a href="/">4350T</a></li>
+								<li><a href="/">4360T</a></li>
+								<li><a href="/">4330</a></li>
+								<li><a href="/">4340</a></li>
+								<li><a href="/">4350</a></li>
+								<li><a href="/">4360</a></li>
+								<li><a href="/">4370</a></li>
+								
+							</ul>
+							
+							<ul class="column">
+								<h4 class="title">Skylake</h4>
+								<li><a href="/">6100T</a></li>
+								<li><a href="/">6300T</a></li>
+								<li><a href="/">6100</a></li>
+								<li><a href="/">6300</a></li>
+								<li><a href="/">6320</a></li>
+								
+							</ul>
+							
+						</ul>
+					</li>
+					
+					<li class="dropdown-top">
+						<a class="dropdown-top" href="/">Core i5</a>
+						<ul class="dropdown-inside">
+							<ul class="column">
+								<h4 class="title">Sandy Bridge</h4>
+								<li><a href="/">2390T</a></li>
+								<li><a href="/">2300</a></li>
+								<li><a href="/">2310</a></li>
+								<li><a href="/">2320</a></li>
+								<li><a href="/">2380P</a></li>
+								<li><a href="/">2400S</a></li>
+								<li><a href="/">2405S</a></li>
+								<li><a href="/">2400</a></li>
+								<li><a href="/">2450P</a></li>
+								<li><a href="/">2500T</a></li>
+								<li><a href="/">2500S</a></li>
+								<li><a href="/">2500</a></li>
+								<li><a href="/">2500K</a></li>
+								<li><a href="/">2550K</a></li>
+							</ul>
+							
+							<ul class="column">
+								<h4 class="title">Ivy Bridge</h4>
+								<li><a href="/">3330S</a></li>
+								<li><a href="/">3330</a></li>
+								<li><a href="/">3450S</a></li>
+								<li><a href="/">3450</a></li>
+								<li><a href="/">3470T</a></li>
+								<li><a href="/">3470S</a></li>
+								<li><a href="/">3470</a></li>
+								<li><a href="/">3475S</a></li>
+								<li><a href="/">3550S</a></li>
+								<li><a href="/">3570T</a></li>
+								<li><a href="/">3570S</a></li>
+								<li><a href="/">3570</a></li>
+								<li><a href="/">3570K</a></li>
+							</ul>
+							
+							<ul class="column">
+								<h4 class="title">Haswell</h4>
+								<li><a href="/">4430S</a></li>
+								<li><a href="/">4430</a></li>
+								<li><a href="/">4440S</a></li>
+								<li><a href="/">4440</a></li>
+								<li><a href="/">4460T</a></li>
+								<li><a href="/">4460S</a></li>
+								<li><a href="/">4460</a></li>
+								<li><a href="/">4570TE</a></li>
+								<li><a href="/">4570T</a></li>
+								<li><a href="/">4570R</a></li>
+								<li><a href="/">4570S</a></li>
+								<li><a href="/">4570</a></li>
+								<li><a href="/">4590T</a></li>
+								<li><a href="/">4590S</a></li>
+								<li><a href="/">4590</a></li>
+								<li><a href="/">4670T</a></li>
+								<li><a href="/">4670R</a></li>
+								<li><a href="/">4670S</a></li>
+								<li><a href="/">4670</a></li>
+								<li><a href="/">4670K</a></li>
+								<li><a href="/">4690T</a></li>
+								<li><a href="/">4690S</a></li>
+								<li><a href="/">4690</a></li>
+								<li><a href="/">4690K</a></li>
+							</ul>
+							
+							<ul class="column">
+								<h4 class="title">Skylake</h4>
+								<li><a href="/">6400</a></li>
+								<li><a href="/">6500</a></li>
+								<li><a href="/">6600</a></li>
+								<li><a href="/">6600K</a></li>
+							</ul>
+							
+							
+						</ul>
+					</li>
+					
+					<li class="dropdown-top">
+						<a class="dropdown-top" href="/">Core i7</a>
+						<ul class="dropdown-inside">
+							
+							<ul class="column">
+								<h4 class="title">Sandy Bridge</h4>
+								<li><a href="/">2600S</a></li>
+								<li><a href="/">2600</a></li>
+								<li><a href="/">2600K</a></li>
+								<li><a href="/">2700K</a></li>
+								<li><a href="/">3820</a></li>
+								<li><a href="/">3930K</a></li>
+								<li><a href="/">3960X</a></li>
+							</ul>
+							
+							<ul class="column">
+								<h4 class="title">Ivy Bridge</h4>
+								<li><a href="/">3770T</a></li>
+								<li><a href="/">3770S</a></li>
+								<li><a href="/">3770</a></li>
+								<li><a href="/">3770K</a></li>
+								
+							</ul>
+							
+							<ul class="column">
+								<h4 class="title">Haswell</h4>
+								<li><a href="/">4765T</a></li>
+								<li><a href="/">4770TE</a></li>
+								<li><a href="/">4770T</a></li>
+								<li><a href="/">4770S</a></li>
+								<li><a href="/">4770R</a></li>
+								<li><a href="/">4770</a></li>
+								<li><a href="/">4770K</a></li>
+								<li><a href="/">4771</a></li>
+								<li><a href="/">4785T</a></li>
+								<li><a href="/">4790T</a></li>
+								<li><a href="/">4790S</a></li>
+								<li><a href="/">4790</a></li>
+								<li><a href="/">4790K</a></li>
+								<li><a href="/">5820K</a></li>
+								<li><a href="/">5930K</a></li>
+								<li><a href="/">5960X</a></li>
+							</ul>
+							
+							<ul class="column">
+								<h4 class="title">Skylake</h4>
+								<li><a href="/">6700</a></li>
+								<li><a href="/">6700K</a></li>
+								
+							</ul>
+							
+						</ul>
+					</li>
+				</ul>
+			</div>
+			
+		</main>
+		
+	</body>  			
